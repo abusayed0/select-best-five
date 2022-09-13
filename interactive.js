f93c5c6ee4bf85986964be485c340f180599cd6b
abusayed0 <abusayed48876@gmail.com>
2022-09-13 16:22:38 +0600

    deligated addevent listener to the parent of calculate and calculate total btn

--- interactive.js
+++ interactive.js
@@ -6,7 +6,8 @@ document.getElementById("players-parent").addEventListener("click", function (ev
         const player = playerSelected.length
         // condition for add maximum 5 
         if (player < 5) {
-            event.target.setAttribute("disabled", true)
+            event.target.setAttribute("disabled", true);
+            event.target.classList.add("btn-secondary")
             const selectedParent = document.getElementById("selected-parent")
             const selectedPlayerName = event.target.parentNode.childNodes[1].innerText;
             const newPlayer = document.createElement("li")
@@ -18,5 +19,18 @@ document.getElementById("players-parent").addEventListener("click", function (ev
             return alert("Only you can choose 5 players")
         }
 
+    }
+})
+
+
+// delegated add event lister to the partent of calculate and calculate total button
+document.getElementById("calculation-parent").addEventListener("click",function(event){
+    if(event.target.tagName == "BUTTON"){
+        if(event.target.id == "calculate"){
+            console.log(event.target.innerText)
+        }
+        else if(event.target.id == "calculate-total"){
+            console.log(event.target.innerText)
+        }
     }
 })
